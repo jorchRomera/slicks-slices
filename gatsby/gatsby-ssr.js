572ede76b598aaec808274328b573862b4b681ec
Jorch <jorge.a.romera@gmail.com>
2021-06-20 04:30:04 -0300

    added styling

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Layout from './src/components/Layout';
 
-const wrapPageElement = ({ element, props }) => (
+export const wrapPageElement = ({ element, props }) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
   <Layout {...props}>{element}</Layout>
 );
-
-export default wrapPageElement;
